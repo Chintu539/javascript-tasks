cf2e64cfa5fe9c756eace71ef38124d864d12060
chintu539 <swamulujanni@gmail.com>
2026-06-08 10:28:56 +0530

    initial commit

---
+++ task1.js
@@ -0,0 +1,112 @@
+/*let marks = 75;
+
+if (marks >= 35) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 60) {
+    console.log("Grade C");
+} else {
+    console.log("No Grade");
+}   */
+
+/*let salary = 25000;
+
+let bonus = salary * 10 / 100;
+
+let finalSalary = salary + bonus;
+
+console.log("Original Salary: " + salary);
+console.log("Bonus: " + bonus);
+console.log("Final Salary: " + finalSalary);*/
+
+/*let email = "admin@gmail.com";
+let password = "12345";
+
+let correctEmail = "admin@gmail.com";
+let correctPassword = "12345";
+
+if (email === correctEmail && password === correctPassword) {
+    console.log("Login Success");
+} else {
+    console.log("Login Failed");
+} */
+
+/* let price = 2000;
+
+let finalAmount = (price > 1000) ? price - (price * 20 / 100) : price;
+
+console.log("Final Amount: " + finalAmount);*/
+
+/*let products = ["Laptop", "Mobile", "Mouse", "Keyboard"];
+
+console.log("First Product: " + products[0]);
+
+console.log("Last Product: " + products[products.length - 1]);
+
+console.log("Total Products: " + products.length);*/
+
+/*let employee = {
+    name: "Naveen",
+    department: "Development",
+    salary: 30000
+};
+
+console.log(employee);
+console.log("Name: " + employee.name);
+console.log("Salary: " + employee.salary);*/
+
+/*let shirt = 500;
+let pant = 1000;
+let shoe = 1500;
+
+let total = shirt + pant + shoe;
+
+let discount = total * 10 / 100;
+
+let finalAmount = total - discount;
+
+console.log("Total: " + total);
+console.log("Discount: " + discount);
+console.log("Final Amount: " + finalAmount);*/
+
+/*let age = 19;
+
+if (age >= 18) {
+    console.log("Eligible for Voting");
+} else {
+    console.log("Not Eligible");
+}*/
+
+/*let a = 10;
+
+console.log("Initial value: " + a);
+
+a++;
+console.log("After a++: " + a);
+
+++a;
+console.log("After ++a: " + a);
+
+a--;
+console.log("After a--: " + a);
+
+--a;
+console.log("After --a: " + a);*/
+
+/*let isPresent = true;
+let completedTask = true;
+
+if (isPresent && completedTask) {
+    console.log("Eligible for Salary");
+} else {
+    console.log("Not Eligible");
+}*/
+
