fdd1d683ff92c6f57251fb3bd7e40c63463be5c4
chintu539 <swamulujanni@gmail.com>
2026-06-12 21:14:10 +0530

    added first.html and first.js

---
+++ first task/first.js
@@ -0,0 +1,60 @@
+/*let studentName = "Naveen";
+let studentAge = 18;
+let studentCourse = "MERN Stack";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);*/
+
+/*var company = "Stackly";
+let salary = 20000
+const country = "India";
+
+company = "Google";
+salary = 30000;
+
+console.log(company);
+console.log(salary);
+console.log(country);*/
+
+/*let userName = prompt("What is your name?");
+console.log("Welcome " + userName);*/
+
+/*let knowsJS = confirm("Do you know JavaScript?");
+console.log(knowsJS);*/
+
+/*let studentName = prompt("Enter Student Name:");
+let studentAge = prompt("Enter Student Age:");
+let studentCity = prompt("Enter Student City:");
+
+console.log(`Name : ${studentName}`);
+console.log(`Age  : ${studentAge}`);
+console.log(`City : ${studentCity}`);*/
+
+/*let course = "JavaScript";
+alert("Welcome to " + course);*/
+
+/*document.writeln("Hello Student");
+document.writeln("Welcome to JavaScript");*/
+
+/*let favoriteFood = "Chicken Biryani";
+let favoriteColor = "Black";
+let favoriteMovie = "Leo";
+
+document.write("Food : " + favoriteFood + "<br>");
+document.write("Color : " + favoriteColor + "<br>");
+document.write("Movie : " + favoriteMovie + "<br>");*/
+
+/*let name = "Naveen";
+let age = 25;
+let phone = "123-456-7890";
+let email = "naveen@example.com";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Phone:", phone);
+console.log("Email:", email);*/
+
+/*let userName = prompt("Enter your Name:");
+let userCourse = prompt("Enter your Course:");
+alert("Hello " + userName + ", Welcome to " + userCourse + " Course");*/
